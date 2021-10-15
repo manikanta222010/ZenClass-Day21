a373c36f1a1dbec419ba7602047d27a0c4372381
manikanta222010 <51736184+manikanta222010@users.noreply.github.com>
2021-10-15 21:31:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let num = document.querySelector("h1").innerText
+setTimeout(() => {
+    document.querySelector("h1").innerText = --num
+    setTimeout(() => {
+        document.querySelector("h1").innerText = --num
+        setTimeout(() => {
+            document.querySelector("h1").innerText = --num
+            setTimeout(() => {
+                document.querySelector("h1").innerText = --num
+                setTimeout(() => {
+                    document.querySelector("h1").innerText = --num
+                    setTimeout(() => {
+                        document.querySelector("h1").innerText = --num
+                        setTimeout(() => {
+                            document.querySelector("h1").innerText = --num
+                            setTimeout(() => {
+                                document.querySelector("h1").innerText = --num
+                                setTimeout(() => {
+                                    document.querySelector("h1").innerText = --num
+                                    setTimeout(() => {
+                                        document.querySelector("h1").innerText = "Happy Independence Day"
+                                    }, 1000 * 1)
+                                }, 1000 * 1)
+                            }, 1000 * 1)
+                        }, 1000 * 1)
+                    }, 1000 * 1)
+                }, 1000 * 1)
+            }, 1000 * 1)
+        }, 1000 * 1)
+    }, 1000 * 1)
+}, 1000 * 1);
